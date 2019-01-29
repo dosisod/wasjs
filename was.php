b1b909c99a863fd969f63b6e3b8d5f6100d72731
dosisod <39638017+dosisod@users.noreply.github.com>
2019-01-28 17:27:36 -0800

    bits can now be file specific

--- was.php
+++ was.php
@@ -3,19 +3,27 @@ session_start();
 
 //Modify these as needed
 
-$path="/absolute/path/to/files/"; //root path to files (dont put in doc root)
-$bits=17; //forces the client to get a POW with n leading bits
+//root path to files (dont put in doc root)
+$path="/absolute/path/to/files/";
 
-//
+//if you want to force different bits for different file types, do that here
+function bits($type) {
+	if ($type=="mp3" || $type=="mp4") return 21;
+	else if ($type="zip" || $type="exe") return 19;
+	
+	return 17; //use this if no other file is found
+}
+
+//end of modify
 
 if (isset($_POST["challenge"], $_POST["file"]) && !isset($_POST["pow"])) { //if user requests challenge
 	$challenge=base64_encode(random_bytes(32)); //create challenge
 
 	$_SESSION["challenge"]=$challenge; //sets session data
 	$_SESSION["file"]=$_POST["file"];
-	$_SESSION["bits"]=$bits;
+	$_SESSION["bits"]=bits(pathinfo($_POST["file"], PATHINFO_EXTENSION));
 
-	echo json_encode(array("bits"=>$bits, "challenge"=>$challenge)); //returns json obj to client
+	echo json_encode(array("bits"=>$_SESSION["bits"], "challenge"=>$challenge)); //returns json obj to client
 }
 else if (isset($_POST["challenge"], $_POST["pow"], $_POST["file"])) { //if user completed challenge
 	if (isset($_SESSION["challenge"], $_SESSION["file"])) { //makes sure session data exists
@@ -25,6 +33,7 @@ else if (isset($_POST["challenge"], $_POST["pow"], $_POST["file"])) { //if user 
 			for ($i=0;$i<intdiv(strlen($hex)+3,4);$i++) { //loop through each char of hex digest
 				$bin.=str_pad(base_convert($hex[$i], 16, 2), 4, "0", STR_PAD_LEFT); //converts hex to bin
 			}
+			$bits=$_SESSION["bits"];
 			if (substr($bin, 0, $bits)==str_repeat("0", $bits)) { //check if leading 0s is >= bits
 				//pow is done, make temp link file
 
