a7c44819f839e4d1e962df4777f48592f571e304
dosisod <39638017+dosisod@users.noreply.github.com>
2020-01-23 23:25:43 -0800

    refactored js file

--- was.js
+++ was.js
@@ -1,102 +1,124 @@
-class was {
-	constructor(current, file) { //pass this to WAS to get the elements id
-		this.file=file //url to query php when found
-		this.php="was.php" //php server to upload to
-		this.node=current.parentNode //gets parent div
+class WAS {
+	constructor(clickedImg, filepath) {
+		this.filepath=filepath
+		this.endpoint="was.php"
+		this.holder=clickedImg.parentNode
 
-		this.img=current //instance is called from inside img
-		this.fspan=document.createElement("SPAN") //file name span
-		this.fspan.innerText=file
-		this.node.append(this.fspan) //appends it to the parent div
+		this.img=clickedImg
+		this.text=document.createElement("SPAN")
+		this.text.innerText=filepath
+		this.holder.append(this.text)
 
-		this.link=document.createElement("a") //hidden link for downloading
-		this.link.style.display="none"
-		this.node.append(this.link)
+		this.hiddenLink=document.createElement("a")
+		this.hiddenLink.style.display="none"
+		this.holder.append(this.hiddenLink)
 
-		this.active=false //prevents double clicking
+		this.started=false
 
-		this.node.onclick=()=>{ if (!this.active) this.run() }
+		this.holder.onclick=()=> {
+			if (!this.started) this.start()
+		}
 
-		//caches imgs
-		this.minerimg=new Image()
-		this.doneimg=new Image()
-		this.errorimg=new Image()
-		this.minerimg.src="mining.gif"
-		this.doneimg.src="done.png"
-		this.errorimg.src="error.png"
+		//lazy load images
+		this.minerImg=new Image()
+		this.doneImg=new Image()
+		this.errorImg=new Image()
+		this.minerImg.src="mining.gif"
+		this.doneImg.src="done.png"
+		this.errorImg.src="error.png"
 	}
-	async run() {
-		this.active=true //makes sure miner isnt being ran more then once
 
-		this.json=await this.challenge() //waits for response from server
-		this.key=this.json["challenge"]
-		this.bits=this.json["bits"]
-		if (this.bits>32||this.bits<0) return //32 or more, or 0 and below are unreasonable, dont mine
-		
-		this.img.onload=()=>this.color("#ff006e")
-		this.img.src=this.minerimg.src //the img will already be in cache
+	async start() {
+		this.started=true
 
-		this.index=1 //starts at 1 since 0 would cause (index%2500==0) to be true
+		const json=await this.getChallenge()
+		const bits=json["bits"]
+		this.key=json["challenge"]
+
+		if (bits<0 || bits>32) {
+			//TODO: display an error
+			return
+		}
 		
-		var mine=()=>{
-			for(;;this.index++) { //loops forever until POW is completed
-				var hash=sha512(this.key+this.index)
-				var digest='' //cast to string
-				for (var j=0;j<(~~((this.bits+3)/4));j++) { //loops through each character of hex digest to create binary digest
-					var dec=parseInt(hash[j], 16) //turns hex to dec
-					var bin=dec.toString(2) //turns hex into binary
-					digest+="0".repeat(4-bin.length)+bin //adds leading 0s, eg turns "10" into "0010"
+		this.img.onload=()=>this.setColor("#ff006e")
+		this.img.src=this.minerImg.src
+
+		this.index=1
+
+		const mine=()=>{
+			for(;;this.index++) {
+				const hash=sha512(this.key + this.index)
+				var digest=""
+
+				//loops through each character of hex digest to create binary digest
+				for (let i=0; i < (~~((bits + 3) / 4)) ; i++) {
+					const binary=parseInt(hash[i], 16).toString(2)
+
+					digest+=("0000" + binary).slice(4)
 				}
-				if (Number(digest.substr(0, this.bits))==0) { //if number of leading zeros is met
+
+				if (Number(digest.substr(0, bits))==0) {
 					this.pow=this.index
-					this.done() //download file
+					this.done()
 					break
 				}
-				if (this.index%2500==0) { //miner must start and stop to prevent "slow script" error
+
+				if (this.index % 2500==0) {
 					setTimeout(mine, 0)
-					this.index++ //makes sure next miner picks up where this one left off
-					break //prevents the miner from running after its interval is over
+					this.index++
+					break
 				}
 			}
 		}
-		mine() //runs until POW is done
+		mine()
 	}
-	async done() { //sends finished POW to server
-		this.img.onload=()=>this.color("#6eff00")
-		this.img.src=this.doneimg.src
+
+	async done() {
+		this.img.onload=()=>this.setColor("#6eff00")
+		this.img.src=this.doneImg.src
 		
 		var form=new FormData()
 		form.append("challenge", this.key)
 		form.append("pow", this.pow)
-		form.append("file", this.file)
+		form.append("file", this.filepath)
 
-		var resp=await fetch(this.php, {method:"post", credentials:"same-origin", body:form})
-			.then(e=>e.text())
-			.then(e=>{
-				if (e.includes("ERROR")) { //if there is an error returned
-					this.img.onload=()=>{
-						this.fspan.innerText=e //show error
-						this.color("#ff0000")
-					}
-					this.img.src=this.errorimg.src //load error img
+		await fetch(this.endpoint, {
+			method:"post",
+			credentials:"same-origin",
+			body: form
+		})
+		.then(e=>e.text())
+		.then(e=> {
+			if (e.includes("ERROR")) {
+				this.img.onload=()=> {
+					this.text.innerText=e
+					this.setColor("#ff0000")
 				}
-				else {
-					this.link.href=e //sets invisible link
-					this.link.click() //clicks the link, downloads in background
-				}
-			})
+				this.img.src=this.errorImg.src
+			}
+			else {
+				this.hiddenLink.href=e
+				this.hiddenLink.click()
+			}
+		})
 	}
-	challenge() { //gets new challenge from server
+
+	getChallenge() {
 		var form=new FormData()
-		form.append("challenge", 1) //second param can be anything, php only checks if challenge is set
-		form.append("file", this.file)
+		form.append("challenge", 1)
+		form.append("file", this.filepath)
 
-		return fetch(this.php, {method:"post", credentials:"same-origin", body:form})
-			.then(e=>e.json())
-			.then(e=>{return e}) //return the text output
+		return fetch(this.endpoint, {
+			method:"post",
+			credentials: "same-origin",
+			body: form
+		})
+		.then(e=>e.json())
+		.then(e=>{ return e })
 	}
-	color(str) { //changes color for different modes etc
-		this.fspan.style.borderBottom="4px solid "+str //line
-		this.fspan.style.color=str //text
+
+	setColor(color) {
+		this.text.style.borderBottom="4px solid "+color
+		this.text.style.color=color
 	}
 }
